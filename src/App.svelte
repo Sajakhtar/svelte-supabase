ab0c7ee36b70c60bac22fe0afaa619aaa4332c86
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-12-14 15:53:18 +0400

    update app.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
 	export let name;
 </script>
 
@@ -27,4 +27,26 @@
 			max-width: none;
 		}
 	}
-</style>
+</style> -->
+
+
+<script>
+    import {user} from "./sessionStore"
+    import {supabase} from "./supabaseClient"
+    import Auth from "./Auth.svelte"
+    import Profile from "./Profile.svelte"
+
+    user.set(supabase.auth.user())
+
+    supabase.auth.onAuthStateChange((_, session) => {
+        user.set(session.user)
+    })
+</script>
+
+<div class="container" style="padding: 50px 0 100px 0;">
+    {#if $user}
+        <Profile />
+    {:else}
+        <Auth />
+    {/if}
+</div>
